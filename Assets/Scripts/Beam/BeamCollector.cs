5d89832e567b8686fe7a6006fe223dd0d15bd81a
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-12 20:54:42 +0700

    Prepare Method for Receive Input

--- Assets/Scripts/Beam/BeamCollector.cs
+++ Assets/Scripts/Beam/BeamCollector.cs
@@ -18,4 +18,24 @@ public class BeamCollector : MonoBehaviour {
     {
         Debug.Log("Add Member { "+"Span : "+span+" Type : "+type+" }");
     }
+
+    public void AddSupport(int type,int node)
+    {
+        Debug.Log("Add Support { " + "type : " + type + " Node : " + node + " }");
+    }
+
+    public void AddPointLoad(int node, float load)
+    {
+        Debug.Log("Add Point Load { " + "node : " + node + " load : " + load + " }");
+    }
+
+    public void AddUniformLoad(int element,float load)
+    {
+        Debug.Log("Add Uniform Load { " + "load : " + load + " element : " + element + " }");
+    }
+
+    public void AddMomentum(int node,float momentum)
+    {
+    }
+        Debug.Log("Add Momentum { " + "Momentum : " + momentum + " node : " + node + "} ");
 }
