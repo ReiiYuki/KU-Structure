429b5c89599cf56f8234272df70512b84dd8624c
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-12 08:15:33 +0700

    Dispose Panel

---
+++ Assets/Scripts/UtilityScript/DisposePanelAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisposePanelAction : MonoBehaviour {
+
+	public void Dispose()
+    {
+        transform.parent.gameObject.SetActive(false);
+    }
+
+}
