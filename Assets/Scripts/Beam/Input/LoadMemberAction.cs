49deccecebabedf9a00f09ac7eec015960a26347
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-13 02:31:21 +0700

    Change Sorting Layer of Dropdown

--- Assets/Scripts/Beam/Input/LoadMemberAction.cs
+++ Assets/Scripts/Beam/Input/LoadMemberAction.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class LoadMemberAction : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	void OnEnable () {
+        LoadMember();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void LoadMember()
+    {
+        Debug.Log("Load Member is working");
+        List<GameObject> memebers = GameObject.FindObjectOfType<BeamCollector>().members;
+        Dropdown memberDropdown = GetComponent<Dropdown>();
+        foreach (GameObject member in memebers)
+        {
+            memberDropdown.options.Add(new Dropdown.OptionData() { text = member.GetComponent<MemberProperty>().number+"" });
+        }
+        memberDropdown.RefreshShownValue();
+    }
+
 }
