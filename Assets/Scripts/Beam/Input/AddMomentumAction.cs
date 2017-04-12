78b858d374bf25a03daa06ba328251a860cfd3c8
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-12 22:26:36 +0700

    Add UniformLoad / Momentum to Beam Collector

---
+++ Assets/Scripts/Beam/Input/AddMomentumAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddMomentumAction : MonoBehaviour {
+
+    public GameObject nodeDropdown, momentumText;
+
+    public void AddMomentum()
+    {
+        int node = nodeDropdown.GetComponent<Dropdown>().value;
+        float momentum = float.Parse(momentumText.GetComponent<Text>().text);
+        GameObject.FindObjectOfType<BeamCollector>().AddMomentum(node, momentum);
+    }
+}
