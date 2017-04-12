b30cf96ea61ee92ed50b7cf2664b97a9403961e6
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-12 08:03:24 +0700

    Show Panel Action

---
+++ Assets/Scripts/UtilityScript/ShowPanelAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPanelAction : MonoBehaviour {
+
+
+    public GameObject panel;
+
+	public void ShowPanel()
+    {
+        panel.SetActive(true);
+    }
+
+}
