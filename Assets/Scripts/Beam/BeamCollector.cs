b31a5237814640dc9f90f95ab5d0a90d0b17db2e
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-12 20:38:34 +0700

    Receive Member Input and Pass to Beam Collector

---
+++ Assets/Scripts/Beam/BeamCollector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamCollector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddMember(float span,float type)
+    {
+        Debug.Log("Add Member { "+"Span : "+span+" Type : "+type+" }");
+    }
+}
