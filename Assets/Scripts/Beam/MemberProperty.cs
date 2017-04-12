d08a4645a1c8cb1ce4c1f0a85b534708df7c9d68
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-12 22:51:29 +0700

    Initialize Member Property

---
+++ Assets/Scripts/Beam/MemberProperty.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemberProperty : MonoBehaviour {
+    public float E, I, length;
+    public int number;
+}
