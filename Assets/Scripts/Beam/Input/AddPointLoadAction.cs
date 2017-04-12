6f62809e40970520e78ea771428e30da8f1687dc
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-12 22:07:27 +0700

    Apply add point Load to add button

--- Assets/Scripts/Beam/Input/AddPointLoadAction.cs
+++ Assets/Scripts/Beam/Input/AddPointLoadAction.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddPointLoadAction : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject nodeDropdown, loadText;
+
+    public void AddPointLoad()
+    {
+        int node = nodeDropdown.GetComponent<Dropdown>().value;
+        float load = float.Parse(loadText.GetComponent<Text>().text);
+        GameObject.FindObjectOfType<BeamCollector>().AddPointLoad(node, load);
+    }
 }
