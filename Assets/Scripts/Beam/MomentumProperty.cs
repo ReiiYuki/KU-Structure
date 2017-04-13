f70c41b3a1c228f3d8c91871c29be751a70e101e
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-13 11:36:51 +0700

    Momentum With Direction

---
+++ Assets/Scripts/Beam/MomentumProperty.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomentumProperty : MonoBehaviour {
+
+    public float momentum;
+    public int node;
+
+    public void UpdateDirection()
+    {
+        if (momentum < 0) transform.localScale = new Vector2(0.1f, 0.1f);
+    } 
+}
