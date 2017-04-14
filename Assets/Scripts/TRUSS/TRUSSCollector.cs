fa179497ec454e68c15b5d632c804bbdfe1ca5e5
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-14 23:37:07 +0700

    Prepare method for add

--- Assets/Scripts/TRUSS/TRUSSCollector.cs
+++ Assets/Scripts/TRUSS/TRUSSCollector.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class TRUSSCollector : MonoBehaviour {
 
+    public List<GameObject> nodes,members;
+
 	// Use this for initialization
 	void Start () {
-		
+        this.nodes = new List<GameObject>();
+        this.members = new List<GameObject>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void AddNode(int x,int y)
+    {
+        Debug.Log("Add Node { x:" + x + " y : " + y + " }");
+    }
+
+    public void AddMember(int node1,int node2,int property)
+    {
+        Debug.Log("Add Member { node1 : " + node1 + " node2 : " + node2 + " property : " + property + " }");
+    }
+
+    public void AddSupport(int type,int node)
+    {
+        Debug.Log("Add Support { type : " + type + " node : " + node + " }");
+    }
+
+    public void AddPointLoad(int node,float loadX,float loadY)
+    {
+        Debug.Log("Add Point Load { node : " + node + " loadX : " + loadX + " loadY : " + loadY + " }");
+    }
 }
