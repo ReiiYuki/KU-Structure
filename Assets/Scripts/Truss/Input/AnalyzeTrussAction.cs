529ee7124c9478e67a7123b0ff8325a77889163f
Norawit Urailertprasert <norawit543@hotmail.com>
2017-04-22 00:29:42 +0700

    fix  truss input bug

---
+++ Assets/Scripts/Truss/Input/AnalyzeTrussAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalyzeTrussAction : MonoBehaviour {
+
+	public void Analyze()
+    {
+        GameObject.FindObjectOfType<TrussAnalyzer>();
+    }
+}
+ 
