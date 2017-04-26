b6be0275a003e562e8d801961d7156bf66795934
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-27 01:10:54 +0700

    Completely Use New Element Prop Selector

--- Assets/Scripts/Beam/MemberProperty.cs
+++ Assets/Scripts/Beam/MemberProperty.cs
@@ -8,18 +8,22 @@ public class MemberProperty : MonoBehaviour {
     public UniformLoadProperty uniformLoad;
     public NodeProperty node1,node2;
     public ElementStore.Element prop;
-    public ElementStore.UElement prop2;
+    public ElementStore.UElement uprop;
 
     float[] E = { 1,200000000,30000000, 30000000 , 90000000 };
     float[] I = { 1,7*Mathf.Pow(10,-4) , 0.00048f,0.00072f, 0.00048f };
 
     public float GetI()
     {
-        return I[type];
+        if (prop.name != null) return prop.ix;
+        else return uprop.I;
+      //  return I[type];
     }
 
     public float GetE()
     {
-        return E[type];
+        if (prop.name != null) return prop.e;
+        else return uprop.E;
+     //   return E[type];
     }
 }
