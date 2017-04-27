dd1ed75665f81d3abb2d3a6ee3987891d6203977
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-27 11:54:55 +0700

    Check Load Status before add

---
+++ Assets/Scripts/Beam/Input/LoadCheckStatus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadCheckStatus : MonoBehaviour {
+
+    public GameObject addButton, loadText, nodeSelector;
+	
+	// Update is called once per frame
+	void Update () {
+        CheckAndUpdate();
+    }
+
+    void CheckAndUpdate()
+    {
+        float load;
+        if (float.TryParse(loadText.GetComponent<Text>().text, out load) && nodeSelector.GetComponent<LoadInputValue>().list.Length >= 0)
+            addButton.GetComponent<Button>().interactable = true;
+        else
+            addButton.GetComponent<Button>().interactable = false;
+    }
+}
