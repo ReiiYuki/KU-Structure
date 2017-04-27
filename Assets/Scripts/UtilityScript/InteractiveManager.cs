a23fef4fd26d7ba9a03b65513d7544326a083443
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-27 09:59:01 +0700

    Disable Panel when dialog show

---
+++ Assets/Scripts/UtilityScript/InteractiveManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InteractiveManager : MonoBehaviour {
+
+    public GameObject buttonPanel;
+
+	public void PausePanel()
+    {
+        foreach (Button button in buttonPanel.GetComponentsInChildren<Button>())
+            button.interactable = false;
+    }
+
+    public void ActivatePanel()
+    {
+        foreach (Button button in buttonPanel.GetComponentsInChildren<Button>())
+            button.interactable = true;
+    }
+}
