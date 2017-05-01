14e830c8b57795a14e8e65f45f1df05bffd2b1bd
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-05-01 20:39:47 +0700

    Add User Property for TRUSS

--- Assets/Scripts/UtilityScript/AddUserPropertyAction.cs
+++ Assets/Scripts/UtilityScript/AddUserPropertyAction.cs
@@ -5,15 +5,26 @@ using UnityEngine.UI;
 public class AddUserPropertyAction : MonoBehaviour {
 
     public GameObject eText, iText,pane;
-
+    public string type;
 	public void AddUserProperty()
     {
         Debug.Log(eText.GetComponent<InputField>().text + " " + iText.GetComponent<InputField>().text);
         float e = float.Parse(eText.GetComponent<InputField>().text);
         float i = float.Parse(iText.GetComponent<InputField>().text);
-        string savedData = PlayerPrefs.GetString("UPROP");
-        savedData += " " + e + "," + i + " ";
-        PlayerPrefs.SetString("UPROP", savedData);
+        if (type == "UT")
+        {
+            string savedData = PlayerPrefs.GetString("UTPROP");
+            savedData += " " + e + "," + i + " ";
+            PlayerPrefs.SetString("UTPROP", savedData);
+        }
+        else
+        {
+            string savedData = PlayerPrefs.GetString("UPROP");
+            savedData += " " + e + "," + i + " ";
+            PlayerPrefs.SetString("UPROP", savedData);
+        }
+        
+        
         pane.GetComponent<LoadElementTypeAction>().LoadElement();
     }
 }
