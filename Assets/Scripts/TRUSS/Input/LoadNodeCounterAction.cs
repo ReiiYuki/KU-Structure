f09d2711a7b036397f78fee094e531670fc09f74
Norawit Urailertprasert <norawit543@hotmail.com>
2017-05-30 11:18:46 -0700

    fix 0 in node panel

--- Assets/Scripts/TRUSS/Input/LoadNodeCounterAction.cs
+++ Assets/Scripts/TRUSS/Input/LoadNodeCounterAction.cs
@@ -7,8 +7,8 @@ public class LoadNodeCounterAction : MonoBehaviour {
 
 	// Use this for initialization
 	void OnEnable() {
-        GetComponent<Text>().text = ""+GameObject.FindObjectOfType<TRUSSCollector>().nodes.Count;
-        Debug.Log(GameObject.FindObjectOfType<TRUSSCollector>().nodes.Count);
+        GetComponent<Text>().text = ""+(GameObject.FindObjectOfType<TRUSSCollector>().nodes.Count+1);
+        Debug.Log(GameObject.FindObjectOfType<TRUSSCollector>().nodes.Count+1);
 	}
 
 }
